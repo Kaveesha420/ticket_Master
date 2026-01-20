2b36aa8da1eff1dc84fa9dccdf1d54c2210ad494
Kaveesha420 <icetkaveesha@gmail.com>
2026-01-20 11:33:46 +0530

    Created Seat Entity class

---
+++ src/main/java/ecom/icet/Model/Entity/Seat.java
@@ -0,0 +1,30 @@
+package ecom.icet.Model.Entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Seat {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
+
+    private String seatNumber;
+
+    @Enumerated(EnumType.STRING)
+    private SeatStatus status;
+
+    private Long heldByUserId;
+    private LocalDateTime holdExpiry;
+}
+
+enum SeatStatus { AVAILABLE, HELD, SOLD }
