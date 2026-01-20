3464a2e914bea36c34cf4af79335498c80bd0b4a
Kaveesha420 <icetkaveesha@gmail.com>
2026-01-20 11:50:35 +0530

    Created AuditLogRepossitory interface

---
+++ src/main/java/ecom/icet/Repository/AuditLogRepossitory.java
@@ -0,0 +1,7 @@
+package ecom.icet.Repository;
+
+import ecom.icet.Model.Entity.AuditLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AuditLogRepossitory extends JpaRepository<Long, AuditLog> {
+}
